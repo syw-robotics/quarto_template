a8b6e0f8a0b41bd06dc773efb9ff5c6144db28ed
Siyuan Wang <12012324@mail.sustech.edu.cn>
2025-06-12 15:38:05 +0800

    add Gelasio font

--- local_fonts.sh
+++ local_fonts.sh
@@ -1,13 +1,35 @@
 #!/usr/bin/env bash
 
 
+# check arguments: quit when no arg
+if [ $# -ne 1 ]; then
+  echo -e "\033[31m === No arguments expected! === \n\033[0m"
+  echo -e "\033[32m Available args: lm, gel\033[0m"
+  echo -e "\033[32m   - lm: Latin Modern Sans\033[0m"
+  echo -e "\033[32m   - gel: Gelasio\033[0m"
+  exit 1
+fi
+
 # check if "main_files" exists
 if [ ! -d "./main_files" ]; then
   echo -e "\033[31m === \"main_files\" directory not found! Run \"./preview.sh\" first === \n\033[0m"
   exit 1
 fi
 
-cp -r ./assets/fonts/latinmodern-sans/ ./main_files/libs/revealjs/dist/theme/fonts/
+# if arg is lm
+if [ "$1" == "lm" ]; then
+  cp -r ./assets/fonts/latinmodern-sans/ ./main_files/libs/revealjs/dist/theme/fonts/
+  echo -e "\033[32m === Moved Latin Modern Sans to main_files === \n\033[0m"
+  exit 0
+fi
+
+# if arg is gel
+if [ "$1" == "gel" ]; then
+  cp -r ./assets/fonts/gelasio/ ./main_files/libs/revealjs/dist/theme/fonts/
+  echo -e "\033[32m === Moved Gelasio to main_files === \n"
+  exit 0
+fi
+
 
 echo -e "\033[32m === Moved latinmodern-sans to main_files === \n\033[0m"
 
